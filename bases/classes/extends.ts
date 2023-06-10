d6b5c09601e23942a389e54521a6581ce782299c
René <diaz.de.sandi.7@gmail.com>
2023-06-09 22:24:57 -0700

    Inheritance, super, extends

---
+++ bases/classes/extends.ts
@@ -0,0 +1,35 @@
+(() => {
+  class Avenger {
+    constructor(public name: string, public realName: string) {
+      console.log("Constructor Avenger llamado");
+    }
+
+    // Private significa que solo se puede acceder desde la clase que se utiliza
+    // Procted da acceso dentro de clases que se extiendad de la clase padre.
+    protected getFullName() {
+      return `${this.name} ${this.realName}`;
+    }
+  }
+
+  class Xmen extends Avenger {
+    // Agregando public variable: tipo en el constructor
+    // se crearía la propiedad otra vez
+    constructor(name: string, realName: string, public isMutant: boolean) {
+      super(name, realName);
+      console.log("Constructor Xmen llamado");
+    }
+
+    // Accediento al método getFullName de la clase Avenger
+    getFullNameXmen() {
+      // Al ser privada no se puede acceder a getFullName,
+      // en su lugar se puede utilizar protected
+      console.log(super.getFullName());
+    }
+  }
+
+  const wolverine = new Xmen("Wolverine", "Logan", true);
+
+  console.log(wolverine);
+
+  wolverine.getFullNameXmen();
+})();
