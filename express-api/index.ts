dd71a872d5b2ca40aaf2059a78335e789facd58d
René <diaz.de.sandi.7@gmail.com>
2023-06-11 18:15:35 -0700

    express + ts

---
+++ express-api/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.status(201).json({
+    ok: false,
+    msg: "Petición vacía",
+  });
+
+  res.json({
+    ok: true,
+    msg: "Todo salió bien",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
